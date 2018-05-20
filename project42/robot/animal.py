460ed9aeae48b5e67dac46a32dec67e85840ec0d
Daniel Bencic <daniel.bencic@gmail.com>
2018-05-20 20:24:29 +0200

    byte to int unpack

--- project42/robot/animal.py
+++ project42/robot/animal.py
@@ -1,13 +1,18 @@
+import struct
+
 """Animal module"""
 
 class Animal:
     """Represents an animal."""
     def __init__(self, config_data):
-        self.lower_color = (int.from_bytes(config_data[0], byteorder='big'), int.from_bytes(config_data[1], byteorder='big'), int.from_bytes(config_data[2], byteorder='big'))
-        self.upper_color = (int.from_bytes(config_data[3], byteorder='big'), int.from_bytes(config_data[4], byteorder='big'), int.from_bytes(config_data[5], byteorder='big'))
-        self.min_contour_size = int.from_bytes(config_data[6], byteorder='big')
-        self.contour_type = int.from_bytes(config_data[7], byteorder='big')
-        self.tolerance_to_middle = int.from_bytes(config_data[8], byteorder='big')
+        self.lower_color = (self.__byte_to_int(config_data[0]), self.__byte_to_int(config_data[1]), self.__byte_to_int(config_data[2]))
+        self.upper_color = (self.__byte_to_int(config_data[3]), self.__byte_to_int(config_data[4]), self.__byte_to_int(config_data[5]))
+        self.min_contour_size = self.__byte_to_int(config_data[6])
+        self.contour_type = self.__byte_to_int(config_data[7])
+        self.tolerance_to_middle = self.__byte_to_int(config_data[8])
 
     def __str__(self):
         return str(self.__class__) + ": " + str(self.__dict__)
+
+    def __byte_to_int(self, byte):
+        return struct.unpack("<i", byte + "\x00\x00\x00")
