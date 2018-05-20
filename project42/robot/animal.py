e69fd5a0c5e6b8a4e2ee6825ccd0438697042f7f
Daniel Bencic <daniel.bencic@gmail.com>
2018-05-20 20:11:08 +0200

    convert byte to int

--- project42/robot/animal.py
+++ project42/robot/animal.py
@@ -3,11 +3,11 @@
 class Animal:
     """Represents an animal."""
     def __init__(self, config_data):
-        self.lower_color = (config_data[0], config_data[1], config_data[2])
-        self.upper_color = (config_data[3], config_data[4], config_data[5])
-        self.min_contour_size = config_data[6]
-        self.contour_type = config_data[7]
-        self.tolerance_to_middle = config_data[8]
+        self.lower_color = (int.from_bytes(config_data[0]), int.from_bytes(config_data[1]), int.from_bytes(config_data[2]))
+        self.upper_color = (int.from_bytes(config_data[3]), int.from_bytes(config_data[4]), int.from_bytes(config_data[5]))
+        self.min_contour_size = int.from_bytes(config_data[6])
+        self.contour_type = int.from_bytes(config_data[7])
+        self.tolerance_to_middle = int.from_bytes(config_data[8])
 
     def __str__(self):
         return str(self.__class__) + ": " + str(self.__dict__)
